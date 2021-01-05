88a6e7446b491f17e519790a953c7793ff27074a
SongHeeJae <gmlwo308@gmail.com>
2021-01-06 00:29:23 +0900

    프로젝트 생성

---
+++ src/main/java/com/kuke/parkingticket/ParkingticketApplication.java
@@ -0,0 +1,13 @@
+package com.kuke.parkingticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuke/parkingticket/ParkingticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuke.parkingticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
