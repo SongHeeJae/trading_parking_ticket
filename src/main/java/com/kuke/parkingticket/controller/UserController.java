800dc51b046eb34f0fae68d100a43f82d5dcb67a
SongHeeJae <gmlwo308@gmail.com>
2021-01-06 21:28:19 +0900

    feat: swagger 적용

---
+++ src/main/java/com/kuke/parkingticket/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.kuke.parkingticket.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "User Controller", tags = {"User"})
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @ApiOperation(value = "모든 회원 조회", notes = "모든 회원을 조회한다.")
+    @GetMapping(value = "/users")
+    public String swaggerTest() {
+        return "test";
+    }
+
+}
