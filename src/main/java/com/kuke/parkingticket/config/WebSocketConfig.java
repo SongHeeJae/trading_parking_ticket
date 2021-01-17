422ddd823c77f4f4b36bb746c5f36b0eaaa1475b
SongHeeJae <gmlwo308@gmail.com>
2021-01-17 19:49:57 +0900

    feat: 소켓 연결 시 jwt 토큰 인증 추가

--- src/main/java/com/kuke/parkingticket/config/WebSocketConfig.java
+++ src/main/java/com/kuke/parkingticket/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.kuke.parkingticket.config;
 
+import com.kuke.parkingticket.handler.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -20,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
---
+++ src/main/java/com/kuke/parkingticket/handler/StompHandler.java
@@ -0,0 +1,29 @@
+package com.kuke.parkingticket.handler;
+
+import com.kuke.parkingticket.config.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        System.out.println("StompHandler.preSend");
+        if(accessor.getCommand() == StompCommand.CONNECT) {
+            if(!jwtTokenProvider.validateToken(accessor.getFirstNativeHeader("token")))
+                throw new AccessDeniedException("");
+        }
+        return message;
+    }
+}
